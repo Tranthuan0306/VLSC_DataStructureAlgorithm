eb8adec291a611e3c5d5fb99641f1e69518ecacf
Tranthuan0306 <ngq313362@gmail.com>
2026-03-30 09:34:43 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Hỗ trợ gõ tiếng Việt có dấu
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+        Console.Write("Nhập họ tên sinh viên: ");
+        string name = Console.ReadLine();
+
+        Console.Write("Nhập mã số sinh viên (MSSV): ");
+        string mssv = Console.ReadLine();
+
+        Console.WriteLine($"\nChào mừng sinh viên {name} (MS: {mssv}) đến với lớp CTDL&GT!");
+    }
+}
